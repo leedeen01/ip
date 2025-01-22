e2c82f80b118048a2389555653d3613d30aabe00
leedeen01 <lmaodeen@gmail.com>
2025-01-22 21:45:09 +0800

    Add the ability to store whatever text entered by the user and display them back to the user when requested

--- src/main/java/Mavis.java
+++ src/main/java/Mavis.java
@@ -1,20 +1,36 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Mavis {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Hello! I'm Mavis\nWhat can I do for you?");
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));        
+        pw.println("Hello! I'm Mavis\nWhat can I do for you?");
+        pw.flush();
+        ArrayList <String> shelf = new ArrayList<>();
         while(true) {
-            String word = scanner.nextLine();
-            if (word.equals("bye")) {
+            String inputs = br.readLine();            
+            if (inputs.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (inputs.equals("list")){
+                if (shelf.isEmpty()) {
+                    pw.println("The shelf is empty.");
+                } else {
+                    for (int i = 0; i < shelf.size(); i++) {
+                        pw.println((i + 1) + ". " + shelf.get(i));
+                    }
+                    pw.flush();
+                }
             } else {
-                System.out.println("Mavis: " + word);
+                shelf.add(inputs);
+                pw.println("added: " + inputs);
+                pw.flush();
             }
         }
-        scanner.close();
+        pw.flush();
+        pw.close();
 
     }
 }
