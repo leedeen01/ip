5be8587a95c8b7f44c6ffa97257f56f39aec67e5
leedeen01 <lmaodeen@gmail.com>
2025-01-22 21:14:37 +0800

    Rename chatbot name to "Mavis"

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Mavis User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Mavis.java
@@ -0,0 +1,5 @@
+public class Mavis {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Mavis\nWhat can I do for you?\n\nBye. Hope to see you again soon!");
+    }
+}
