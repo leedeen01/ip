324733dc85fa62927d5ce585ae6d54f7ca01c1ce
leedeen01 <lmaodeen@gmail.com>
2025-01-22 21:23:36 +0800

    Add echoing feature to Mavis

--- src/main/java/Mavis.java
+++ src/main/java/Mavis.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Mavis {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Mavis\nWhat can I do for you?\n\nBye. Hope to see you again soon!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello! I'm Mavis\nWhat can I do for you?");
+
+        while(true) {
+            String word = scanner.nextLine();
+            if (word.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println("Mavis: " + word);
+            }
+        }
+        scanner.close();
+
     }
 }
